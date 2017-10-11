3baa1b45e84ff1190b008e2925acb85f7bd0ca4c
deliaslan <deliaslan@gmail.com>
2017-10-11 22:17:38 +0300

    first commit

--- PhpstormProjects/calisma01/php02.php
+++ PhpstormProjects/calisma01/php02.php
@@ -16,20 +16,25 @@
 
 $numara1=44;
 $numara2=13;
-
+echo "Toplama İşlemi: ";
 $toplam = $numara1+$numara2;
 echo $toplam;
 echo "<br>";
+echo "Çıkarma İşlemi: ";
 $cikar = $numara1 - $numara2;
 echo $cikar;
 echo "<br>";
+echo "Bölme İşlemi: ";
 $bolme = $numara1/$numara2;
 echo $bolme;
 echo "<br>";
+echo "Çarpma İşlemi: ";
 $carpim = $numara1*$numara2;
 echo $carpim;
 echo "<br>";
+echo "Mod Alma İşlemi: ";
 $modAlma = $numara1%$numara2;
 echo $modAlma;
 echo "<br>";
+echo "İşlem adımlarını göstermek için kullanılan yöntem=> $numara1+$numara2 = $toplam"; //böyle bir kullanımda var tüm elemanları yazdırmak için
 ?>
