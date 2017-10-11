a3973369de6dcb3b4bd18fa1f11f8a25a2efe1f4
deliaslan <deliaslan@gmail.com>
2017-10-11 21:18:31 +0300

    first commit

--- PhpstormProjects/calisma01/php01.php
+++ PhpstormProjects/calisma01/php01.php
@@ -1,21 +1,53 @@
+<?php
+
+error_reporting(0);
+?>
 <!doctype html>
 <html lang="tr">
 <head>
     <meta charset="UTF-8">
     <title>PHP Öğrenmeye Başlıyorum...01</title>
 </head>
-    <body>
+<body>
 <!-- PHP -->
-    <?php
-    //Bu php için yorum satırıdır.
-    /*Bu PHP
-     * çoklu yorum satırı içindir.
-     * */
-    # bu da yorum satırı için kullanılır.
-    echo "Adem DELİASLAN";
-    ?><br>
+<?php
+//Bu php için yorum satırıdır.
+/*Bu PHP
+ * çoklu yorum satırı içindir.
+ * */
+# bu da yorum satırı için kullanılır.
+echo "Adem DELİASLAN";
+?><br>
 <!-- ECHO -->
-    echo "Adem DELİASLAN";
+<?php echo "Tek satırda tüm komut satırı varsa virgül ile bitmesine gerek yoktur."  ?>
+<!-- . ile birleştirme işlemi gerçekleştirilir.  -->
+<?php echo "Tek satırda tüm komut satırı varsa virgül ile bitmesine gerek yoktur"." |}| bu cümle . işaretinden sonra yazılan 
+ kısımdır";
 
+echo "merhaba" ?>
+
+
+<hr>
+<br><br>
+<?php
+/*
+-Değişkenler $ işareti ile başlar.
+-Değer ataması için = işareti kullanılır.
+-Atama yapılırken string metinsel ifadeler " " arasında yapılır.
+-Değişkenler rakamla başlayamaz
+-Değişkenlerde boşluk bırakılamaz
+-Türkçe karakterler değişken isimlerinde kullanılamaz.
+-ÖZel karakterleri değişkenleri yine kullanamayız.
+
+*/
+//satır inme şekilleri aşağıda gösterildi.
+echo $ad = "Adem"."<br>";
+echo $soyad = "Deliaslan";
+echo "<br>";
+echo $no = 238;
+echo "<br />";
+echo $okul = "YEAL";
+
+?>
 </body>
 </html>
