76eef0a10e3008d408d0a77cdb47791be4310d86
deliaslan <deliaslan@gmail.com>
2017-10-12 21:23:18 +0300

    php04 hazır fonksiyonlar, strlen, strtolow, strtoupper, substring php05 diziler, <pre> html etiketi kullanımı

---
+++ PhpstormProjects/calisma01/php06.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: deliaslan
+ * Date: 12.10.2017
+ * Time: 16:18
+ * Project Name: calisma01
+ * File Name: php06.php
+ */
+
+//Dizilerde kullanılan bazı hazır fonksiyonlar
+
+
+// sort sıralama fonksiyonu varsayılan küçükten büyüğe sıralama yapar.
+
+$dizi=array(1,2,12,94,35,86,17,8,9,10);
+echo "<pre>";
+print_r($dizi);
+echo "</pre>";
+echo "<br>";
+echo "<hr>";
+sort($dizi);
+echo "<pre>";
+print_r($dizi);
+echo "</pre>";
+echo "<br>";
+echo "<hr>";
+rsort($dizi);
+echo "<pre>";
+print_r($dizi);
+echo "</pre>";
+?>
