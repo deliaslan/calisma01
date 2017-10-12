1c78918bf402b543f63b5f0885c85fb4578b427f
deliaslan <deliaslan@gmail.com>
2017-10-12 11:18:22 +0300

    first commit

--- PhpstormProjects/calisma01/php04.php
+++ PhpstormProjects/calisma01/php04.php
@@ -8,5 +8,25 @@
  * File Name: php04.php
  */
 
+/*
+ * Hazır fonksiyonlar
+*/
 
+//rastgele sayı üretme fonksyionu
+echo rand(1,30);  //1 ile 30 dahil arasında rastgele sayı üretecektir. İlk değer minimum, ikinci değer maksimumu ifade eder.
+echo "<hr>";
+//rastgele üretilen sayıyla bir tahmin yapma uygulaması
+echo $tahminet=rand(1,20); echo "<br>";
+echo "<hr>";
+if($tahminet==7){
+    echo "Sayımız $tahminet ve kazandınız.";
+    echo "<br>";
+    echo "<hr>";
+}
+else{
+    echo "Çıkan sonuç: $tahminet ama bu sayı 7'ye eşit değil!";
+    echo "<br>";
+    echo "<hr>";
+
+}
 ?>
