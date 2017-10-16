05c71064c8655885c44fb42589b7d7d7ce1031e3
deliaslan <deliaslan@gmail.com>
2017-10-16 20:28:56 +0300

    php21 Fonksiyonlarda varsayılan değer atama ile eksik parametrelerin varsayılan değerle return edilmesini sağlayabiliriz.

---
+++ PhpstormProjects/calisma01/php21.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: deliaslan
+ * Date: 16.10.2017
+ * Time: 20:20
+ * Project Name: calisma01
+ * File Name: php21.php
+ */
+?>
+<!doctype html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <title>Fonksiyonlarda varsayılan değer atama</title>
+</head>
+<body>
+<?php
+/*
+ * Fonksiyonlarda varsayılan değer atama ile eksik parametrelerin varsayılan değerle return edilmesini sağlayabiliriz.
+*/
+
+function adSoyad($ad = "ad", $soyad = "soyad")
+{
+
+    return $ad . " " . $soyad;
+}
+
+echo adSoyad("adem");
+?>
+
+</body>
+</html>
